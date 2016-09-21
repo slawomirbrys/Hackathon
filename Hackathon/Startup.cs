9c7b8b8dae7467d665d60ebd7350fe787a288b6f
Sławomir Bryś <slawomir.brys@gmail.com>
2016-09-21 17:55:45 +0200

    Fix Hackathon project by removing unnecessary dependencies.

--- Hackathon/Startup.cs
+++ Hackathon/Startup.cs
@@ -1,14 +1,11 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(Hackathon.Startup))]
 namespace Hackathon
 {
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
         }
     }
 }
