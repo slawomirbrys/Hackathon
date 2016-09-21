fbef7c78d5ec04ad4a283e7ab11c07bc428710ff
Sławomir Bryś <slawomir.brys@gmail.com>
2016-09-21 18:17:51 +0200

    Created base controllers and index sites, fixed links.

---
+++ Hackathon/Controllers/BankMobileController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hackathon.Controllers
+{
+    public class BankMobileController : Controller
+    {
+        // GET: BankMobile
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
