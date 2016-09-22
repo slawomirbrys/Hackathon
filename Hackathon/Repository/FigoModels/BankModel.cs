6eb2dc00da51c461acbcac71378f6c91f29026ec
tomaszgalus <internetum@gmail.com>
2016-09-22 14:26:13 +0200

    Extendig FigoRepository class, add GetAvailableBanks method

---
+++ Hackathon/Repository/FigoModels/BankModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hackathon.Repository.FigoModels
+{
+    public class BankModel
+    {
+        public string BankName { get; set; }
+        public string BankId { get; set; }
+        //TG - ToDo!
+        public string Token { get; set; } 
+    }
+}
--- Hackathon/Repository/FigoRepository.cs
+++ Hackathon/Repository/FigoRepository.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Hackathon.Repository.FigoModels;
 
 namespace Hackathon.Repository
 {
     public class FigoRepository
     {
+        IEnumerable<BankModel> GetAvailableBanks(BankToken bankToken)
+        {
+            return new List<BankModel>()
+            {
+                new BankModel(),
+                new BankModel(),
+                new BankModel(),
+                new BankModel()
+            };
+        }
     }
 }
