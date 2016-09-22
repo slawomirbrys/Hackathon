8636c279938999b779fb3d321fb77116c274c9af
Sławomir Bryś <slawomir.brys@gmail.com>
2016-09-22 17:01:50 +0200

    Fix for figo api routing.

--- Hackathon/App_Start/WebApiConfig.cs
+++ Hackathon/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace Hackathon
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
